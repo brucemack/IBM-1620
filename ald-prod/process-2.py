4d9de98338a9048fa4629c592c27d7f04d2d9afa
brucemack <bruce@mackinnon.com>
2024-06-04 15:09:29 -0400

    Saving metadata

--- ald-prod/process-2.py
+++ ald-prod/process-2.py
@@ -6,9 +6,9 @@ from tkinter import simpledialog
 import os 
 import util 
 import math
+import json
 
 in_base_dir = "/home/bruce/host/IBM-1620/f_ald/png"
-in_fn = "page_82.png"
 
 canvas = None
 tk_image = None
@@ -19,7 +19,6 @@ helv24 = None
 image_origin = (0, 0)
 canvas_size = (1800, 1000)
 scale = 0.7
-rotation = 0
 rotation_tick = 0.1
 
 anchor_point = (0, 0)
@@ -27,11 +26,42 @@ press_1_state = False
 hair_point = (0, 0)
 tk_hair_objects = []
 
+image_type = 0
+image_name = None 
+image_rotation = 0
+
+def load_image(n):
+    global in_fn, original_image, image_type, image_rotation, image_name
+    image_name = n
+    image_rotation = 0
+    image_type = 0
+    # Load image
+    original_image = Image.open(in_base_dir + "/" + image_name + ".png")
+
+    # Load metadata 
+    if os.path.exists(in_base_dir + "/" + image_name + ".json"):
+        with open(in_base_dir + "/" + image_name + ".json", "r") as f:
+            meta = json.load(f)
+            image_rotation = meta["image_rotation"]
+            image_type = meta["image_type"]
+
+def save_image_meta():
+    global image_name, image_type, image_rotation, in_base_dir
+    # Save metadata 
+    meta = {
+        "image_name": image_name,
+        "image_type": image_type,
+        "image_rotation": image_rotation
+    }
+    print("Saving ...")
+    with open(in_base_dir + "/" + image_name + ".json", "w+") as f:
+        json.dump(meta, f)
+
 def update_image():
-    global scale, original_image, tk_transformed_image, image_origin
+    global scale, original_image, tk_transformed_image, image_origin, image_rotation
     new_size = (int(original_image.size[0] * scale), int(original_image.size[1] * scale))
     transformed_image = original_image.resize(new_size)
-    transformed_image = transformed_image.rotate(angle=rotation)
+    transformed_image = transformed_image.rotate(angle=image_rotation)
     # IMPORTANT: DON'T LET THIS GO OUT OF SCOPE!
     tk_transformed_image = ImageTk.PhotoImage(transformed_image)
 
@@ -44,8 +74,13 @@ def redraw_image():
     # Draw new version 
     tk_image = canvas.create_image(image_origin[0], image_origin[1], anchor=tk.NW, image=tk_transformed_image)
 
+def screen_to_design(point):
+    global scale, image_origin
+    # Determine scale 
+    return ((point[0] - image_origin[0]) / scale, (point[1] - image_origin[1]) / scale)
+
 def redraw_hair():
-    global tk_hair_objects, canvas, canvas_size
+    global tk_hair_objects, canvas, canvas_size, image_type
     # Undraw
     for o in tk_hair_objects:
         canvas.delete(o)
@@ -53,6 +88,22 @@ def redraw_hair():
     # Redraw
     tk_hair_objects.append(canvas.create_line((hair_point[0], 0), (hair_point[0], canvas_size[1]), fill="black"))
     tk_hair_objects.append(canvas.create_line((0, hair_point[1]), (canvas_size[0], hair_point[1]), fill="black"))
+
+    # Text
+    design_point = screen_to_design(hair_point)
+    cue = "[" + str(int(design_point[0])) + ", " + str(int(design_point[1])) + "] "
+    tk_hair_objects.append(canvas.create_text(
+        hair_point[0] + 20, hair_point[1] + 20, anchor=tk.NW, 
+        text=cue, fill='#119999', font=helv16))
+    
+    if image_type == 0:
+        txt = "Normal Logic"
+    elif image_type == 1:
+        txt = "Text"
+    else:
+        txt = "Other"
+    tk_hair_objects.append(canvas.create_text(
+        10, 5, anchor=tk.NW, text=txt, fill="red", font=helv16))
 
 def on_motion(event):
     global hair_point, press_1_state, anchor_point, image_origin
@@ -91,18 +142,34 @@ def on_pagedown(event):
     redraw_hair()
 
 def on_key_q(event):
-    global scale, rotation
-    rotation = rotation + rotation_tick
+    global scale, image_rotation
+    image_rotation = image_rotation + rotation_tick
     update_image()
     redraw_image()
     redraw_hair()
 
 def on_key_w(event):
-    global scale, rotation
-    rotation = rotation - rotation_tick
+    global scale, image_rotation
+    image_rotation = image_rotation - rotation_tick
     update_image()
     redraw_image()
     redraw_hair()
+
+def on_f3(event):
+    global image_type, rotation
+    # Save metadata 
+    save_image_meta()
+    quit()
+
+def on_f2(event):
+    global image_type 
+    if image_type == 0:
+        image_type = 1
+    elif image_type == 1:
+        image_type = 2
+    elif image_type == 2:
+        image_type = 0
+    redraw_hair()   
 
 # Setup TK
 root = tk.Tk()
@@ -120,9 +187,11 @@ root.bind("<Prior>", on_pageup)
 root.bind("<Next>", on_pagedown)
 root.bind("q", on_key_q)
 root.bind("w", on_key_w)
+root.bind("<F2>", on_f2)
+root.bind("<F3>", on_f3)
 
-# Load image
-original_image = Image.open(in_base_dir + "/" + in_fn)
+in_fn = "page_82"
+load_image(in_fn)
 
 update_image()
 redraw_image()
