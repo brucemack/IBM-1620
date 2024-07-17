1d2115a1a1f63555defb6fdd0921aa65e7bcfe80
brucemack <bruce@mackinnon.com>
2024-07-17 11:59:57 -0400

    Working on expression tree

--- alder-c2/logicbox.py
+++ alder-c2/logicbox.py
@@ -1,46 +1,119 @@
 import lark
 
+def make_global_name(namespace: str, local_name: str):
+    return namespace + "." + local_name
+
+# ----- Expression Related ---------------------------------------------------
+
+class Value: 
+
+    def __init__(self, v):
+        self.value = v
+
+    def get_bool(self):
+        return bool(self.value)
+
+class Expression:
+    def evaluate(self, namespace: str, variable_declarations, \
+                 value_state: dict[str, Value]) -> Value:
+        raise Exception("Not implemented")
+    
+class Variable(Expression):
+
+    def __init__(self, local_name):
+        self.local_name = local_name 
+
+    def evaluate(self, namespace: str, variable_declarations: dict[str, Expression], \
+                 value_state: dict[str, Value]) -> Value:
+        global_name = make_global_name(namespace, self.local_name)
+        # If the variable has not been computed in this cycle then perform the 
+        # computation and hold the result for next time.
+        if not global_name in value_state:
+            exp = variable_declarations[global_name]
+            value_state[global_name] = exp.evaluate(namespace, variable_declarations, value_state)
+        return value_state[global_name]
+
+class Constant(Expression):
+
+    def __init__(self, value):
+        self.value = value
+
+    def evaluate(self, namespace: str, variable_declarations, \
+                 value_state: dict[str, Value]) -> Value:
+        return self.value
+
+class BinaryExpresion(Expression):
+
+    def __init__(self, type, lhs, rhs):
+        self.type = type 
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def evaluate(self, namespace: str, variable_declarations, \
+                 value_state: dict[str, Value]) -> Value:
+        lhs_value = self.lhs.evaluate(namespace, variable_declarations, value_state)
+        rhs_value = self.rhs.evaluate(namespace, variable_declarations, value_state)
+        return self.eval(lhs_value, rhs_value)
+    
+    def eval(self, lhs: Value, rhs: Value) -> Value:
+        if self.type == "or":
+            return Value(lhs.get_bool() or rhs.get_bool())
+        elif self.type == "and":
+            return Value(lhs.get_bool() and rhs.get_bool())
+        else:
+            raise Exception("Invalid operation type")
+
+class UnaryExpresion(Expression):
+
+    def __init__(self, type, lhs):
+        self.type = type 
+        self.lhs = lhs
+
+    def evaluate(self, namespace: str, variable_declarations, \
+                 value_state: dict[str, Value]) -> Value:
+        lhs_value = self.lhs.evaluate(namespace, variable_declarations, value_state)
+        return self.eval(lhs_value)
+    
+    def eval(self, lhs: Value)-> Value:
+        if self.type == "not":
+            return Value(not lhs.get_bool())
+        else:
+            raise Exception("Invalid operation type")
+        
+
+
+
+
 class Module:
-    def __init__(self, name, parameters):
+    def __init__(self, name, ports, body):
         self.name = name
-        self.parameters = parameters
+        self.ports = ports
+        self.body = body
 
-class ExecutableStatement:
-    def __init__(self):
-        pass
-    def execute(self, value_map, next_value_map):
-        pass
+    def __repr__(self):
+        return "Module: " + self.name + " " + str(self.ports) + " " + str(self.body)
 
-class ExecutableAssignmentStatement(ExecutableStatement):
-    def __init__(self, target_name, exp_node):
-        pass
-    def execute(self, value_map, next_value_map):
-        pass
 
-class ExecuableExpressionNode:
-    def __init__(self, target_name, exp_node):
-        pass
-    def evaluate(self, value_map):
-        pass
+class AssignmentStatement:
 
-class Declaration:
+    def __init__(self, lhs_name, type, rhs_exp):
+        self.lhs_name = lhs_name
+        self.type = type 
+        self.rhs_exp
+
+    def execute(self, namespace: str, value_state):
+        pass 
+
+class PortDeclaration:
 
     def __init__(self, io, wr, name):
         self.io = io
         self.wr = wr 
         self.name = name
 
-    def fill_in(self, d):
-        if self.io is None:
-            self.io = d.io
-        if self.wr is None:
-            self.wr = d.wr
- 
-    def override(self, d):
-        if not d.io is None:
-            self.io = d.io
-        if not d.wr is None:
-            self.wr = d.wr
+    def fill_in(self, other):
+        if self.io is None: self.io = other.io
+        if self.wr is None: self.wr = other.wr
 
     def __repr__(self) -> str:
         io = self.io 
@@ -51,157 +124,85 @@ class Declaration:
             wr = "?"
         return io + "/" + wr + "/" + self.name
 
+class Declaration:
+
+    def __init__(self, wr, name):
+        self.wr = wr 
+        self.name = name
+
+    def __repr__(self) -> str:
+        wr = self.wr 
+        if wr is None:
+            wr = "?"
+        return wr + "/" + self.name
+
 # ----- Parse Tree Transformers ---------------------------------------------------------
 
 class ModuleDeclarationProcessor(lark.visitors.Transformer):
 
-    def __init__(self, modules):
-        self.modules = modules
+    def __init__(self):
+        pass
 
-    # moduledeclaration: MODULE IDENTIFIER "(" identifierlist ")" ";" statementlist ENDMODULE
-        #                          [1]              [2]                   [3]
     def moduledeclaration(self, tree):
-        module_name = str(tree[1])
-        declaration_list = []
-        # Process the identifiers
-        last_declaration = Declaration( "INPUT", "WIRE", None)
-        for decl in tree[2]:
-            # Handle defaults by carrying over the previous characteristic
-            decl.fill_in(last_declaration)
-            declaration_list.append(decl)
-            last_declaration = decl
-        # Process the statements looking for declarations
-        for statement in tree[3]:
-            if statement.data == "statement_declaration":
-                for d in statement.children:
-                    for decl in d:
-                        # Scan to look for name matches
-                        found = False
-                        for possible in declaration_list:
-                            if decl.name ==  possible.name:
-                                possible.override(decl)
-                                found = True
-                        if not found:
-                            declaration_list.append(decl)
-            elif statement.data == "statement_assignment":
-                pass
-            elif statement.data == "statement_moduleinstantiation":
-                pass
+        # Carry forward declaration information as needed
+        previous_pd = PortDeclaration("INPUT", "WIRE", None)
+        for pd in tree[2]:
+            pd.fill_in(previous_pd)
+            previous_pd = pd
+        return Module(str(tree[1]), tree[2], tree[3])
 
-        # Deal with declaration defaults
-        print("BEFORE",declaration_list)
-        for decl in declaration_list:
-            decl.fill_in(Declaration("LOCAL", "WIRE", None))
-        print("AFTER",declaration_list)
-            
-        self.modules[module_name] = Module(module_name, None)
+    def portdeclaration_full(self, tree):
+        return PortDeclaration( tree[0].type, tree[1].type, str(tree[2]) )
 
-    def statementlist_add(self, tree):
+    def portdeclaration_default(self, tree):
+        return PortDeclaration( None, None, str(tree[0]) )
+
+    # This will be a list of port declarations
+    def portdeclarationlist_start(self, tree):
+        r =  [ tree[0] ]
+        return r
+
+    def portdeclarationlist_add(self, tree):
         l = tree[1].copy()
         l.insert(0, tree[0])
         return l
 
-    def statementlist_start(self, tree):
-        return [ tree[0] ]
-    
-    def declarationidentifier_a(self, tree):
-        return Declaration( tree[0].type, tree[1].type, str(tree[2]) )
+# This processor goes through and extracts all of the declarations
+class DeclarationProcessor(lark.visitors.Transformer):
 
-    def declarationidentifier_b(self, tree):
-        return Declaration( tree[0].type, None, str(tree[1]) )
+    def __init__(self):
+        pass
 
-    def declarationidentifier_c(self, tree):
-        return Declaration( None, tree[0].type, str(tree[1]) )
-    
-    def declarationidentifier_d(self, tree):
-        return Declaration( None, None, str(tree[0]) )
-
-    # This will be a list of Declarations
-    def declarationidentifierlist_start(self, tree):
-        return [ tree[0] ]
-
-    def declarationidentifierlist_add(self, tree):
-        l = tree[1].copy()
-        l.insert(0, tree[0])
-        return l
-
-    def declaration_input(self, tree):
-        result = []
-        for id in tree[1]:
-            result.append(Declaration( "INPUT", None, id) )
-        return result
-    
-    def declaration_input_wire(self, tree):
-        result = []
-        for id in tree[2]:
-            result.append(Declaration( "INPUT", "WIRE", id) )
-        return result
-
-    def declaration_output(self, tree):
-        result = []
-        for id in tree[1]:
-            result.append(Declaration( "OUTPUT", None, id))
-        return result
-    
-    def declaration_output_wire(self, tree):
-        result = []
-        for id in tree[2]:
-            result.append(Declaration( "OUTPUT", "WIRE", id))
-        return result
-    
-    def declaration_output_reg(self, tree):
-        result = []
-        for id in tree[2]:
-            result.append(Declaration( "OUTPUT", "REG", id))
-        return result
+    def declaration_wire(self, tree):
+        return [ Declaration("WIRE", x) for x in tree[1] ]
 
     def declaration_reg(self, tree):
-        result = []
-        for id in tree[1]:
-            result.append(Declaration( None, "REG", id))
-        return result
-    
-    def declaration_wire(self, tree):
-        result = []
-        for id in tree[1]:
-            result.append(Declaration( None, "WIRE", id))
-        return result
+        return [ Declaration("REG", x) for x in tree[1] ]
 
-    # Start a list of strings
     def identifierlist_start(self, tree):
         return [ str(tree[0]) ]
 
-    # Continue a list of strings
     def identifierlist_add(self, tree):
         l = tree[1].copy()
         l.insert(0, str(tree[0]))
-        return l  
-
-"""
-class DeclarationProcessor(lark.visitors.Transformer):
-
-    def __init__(self, input_names, reg_names):
-        self.input_names = input_names
-        self.reg_names = reg_names
-        self.stack = []
-
-    def identifierlist_start(self, tree):
-        return [ tree[0] ]
-
-    def identifierlist_add(self, tree):
-        l = tree[1].copy()
-        l.insert(0, tree[0])
         return l
 
-    def declaration_input(self, tree):
-        self.input_names.extend(tree[1])
+    def statement_declaration(self, tree):
+        return tree[0]
 
-    def declaration_reg(self, tree):
-        self.reg_names.extend(tree[1])
+    def statement_assignment(self, tree):
+        return [ ]
 
-    def IDENTIFIER(self, tree):
-        return str(tree)
-"""
+    def statement_moduleinstantiation(self, tree):
+        return [ ]
+
+    def statementlist_start(self, tree):
+        return tree[0]
+
+    def statementlist_add(self, tree):
+        l = tree[1].copy()
+        l.extend(tree[0])
+        return l
 
 class Evaluator(lark.visitors.Transformer):
 
@@ -264,7 +265,7 @@ significant_angles = [ 0, 1, 2, 3,
                     299, 300, 301, 302,
                     309, 310, 311, 312,
                     359 ]
-
+"""
 class LogicBox:
 
     def __init__(self, logic_file_name, value_source):
@@ -278,7 +279,7 @@ class LogicBox:
         # Process declarations (one time)
         self.input_names = []
         self.reg_names = []
-        d = DeclarationProcessor(self.input_names, self.reg_names)
+        d = ModuleDeclarationProcessor(self.input_names, self.reg_names)
         d.transform(self.tree)
         #print("Inputs:", self.input_names)
         #print("Regs:", self.reg_names)
@@ -328,3 +329,4 @@ class LogicBox:
             self.value_map[n] = v
 
         self.tick_count = self.tick_count + 1
+"""
--- alder-c2/test_logicparser.py
+++ alder-c2/test_logicparser.py
@@ -1,25 +1,19 @@
 import lark
 import logicbox
 
-# Parse some contructs 
 def test_1():
 
     parser = lark.Lark.open("./logic.lark")
     tree = parser.parse(
     """
     // Define the module
-    module test1(a, b);
-        input a;
-        reg b;
-        output b;
-        output reg e, f;
-        b = a;
-        b = a;
-        b = a;
-        b = a;
+    module test1(input wire a, b);
+        reg d;
+        wire c, e, f;
+        d = a;
     endmodule
     // Define another module
-    module main(x);
+    module main(output reg x);
         a = b;
         // Instantiate the module
         test1 test1inst(.a(a1), .b(b1));
@@ -27,9 +21,40 @@ def test_1():
     """)
     print(tree.pretty())
 
-    modules = {}
-    d = logicbox.ModuleDeclarationProcessor(modules)
-    d.transform(tree)
-    print(modules)
+    d = logicbox.ModuleDeclarationProcessor()
+    for module in d.transform(tree).children:
+        print("-----", module, "--------")
+        t0 = logicbox.DeclarationProcessor()
+        print("XXX", t0.transform(module.body))
 
-test_1()
+def test_2():
+
+    decls = {}
+    states = {}
+
+    x0 = logicbox.BinaryExpresion("and", logicbox.Constant(logicbox.Value(True)), \
+                                  logicbox.Constant(logicbox.Value(True)))
+    assert x0.evaluate("root", decls, states).get_bool()
+
+    x1 = logicbox.BinaryExpresion("and", logicbox.Constant(logicbox.Value(True)), \
+                                  logicbox.Constant(logicbox.Value(False)))
+    assert not x1.evaluate("root", decls, states).get_bool()
+
+    states["root.a"] = logicbox.Value(True)
+    
+    x2 = logicbox.BinaryExpresion("and", logicbox.Constant(logicbox.Value(True)), \
+                                  logicbox.Variable("a"))
+    assert x2.evaluate("root", decls, states).get_bool()
+
+    # Put the last expression into the declaration map
+    decls["root.b"] = x2
+
+    x3 = logicbox.UnaryExpresion("not", logicbox.Variable("b"))
+    assert not x3.evaluate("root", decls, states).get_bool()
+
+
+
+
+
+
+test_2()
